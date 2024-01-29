e5b9d2f56a3e3b8f0e3e8a715ee2ee04ea9648b0
nishant <nishant@gmail.com>
2024-01-29 08:18:57 +0530

    added a node js application

--- Basic-Js/index.js
+++ Basic-Js/index.js
@@ -15,3 +15,16 @@ console.log(name);
 // const allows us to create a constant
 const id = "a123"
 console.log(id);
+
+var multiply = function(x,y) {
+    z = x * y;
+    return z;
+} 
+// traditional method to create a function
+
+
+// ec6 arrow function 
+const add = (x,y) => x + y;
+
+console.log(multiply(10,20));
+console.log(add(10,7));
