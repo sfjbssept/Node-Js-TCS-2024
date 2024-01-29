a9cd1c32c42e00e8fc9bd8a174754b3ef71a35b8
nishant <nishant@gmail.com>
2024-01-29 07:16:34 +0530

    added variables and data types

---
+++ Basic-Js/index.js
@@ -0,0 +1,17 @@
+var name = "nishant" // string
+var age = 33 // number
+var isAdult = true //
+// var allows us to define variable in the whole program
+
+console.log(name);
+{
+    let name = "Peter"
+    console.log(name);
+    // let allows us to create a variable in blocked scope
+
+}
+console.log(name);
+
+// const allows us to create a constant
+const id = "a123"
+console.log(id);
